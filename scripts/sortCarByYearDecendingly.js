173bcd4fa886b2a819ba4ad4579639d77b261b6e
Erick Yudha <erickyudha18@gmail.com>
2022-08-31 20:34:47 +0700

    done sort desc function

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -8,6 +8,19 @@ function sortCarByYearDescendingly(cars) {
 
   // Tulis code-mu disini
 
+  // INSERTION SORT APPROACH
+  for (let i = 1; i < result.length; i++) {
+    let currentCar = result[i];
+    let prevCarIndex = i - 1;
+
+    while (prevCarIndex >= 0 && currentCar.year > result[prevCarIndex].year) {
+      result[prevCarIndex + 1] = result[prevCarIndex];
+      prevCarIndex--;
+    }
+
+    result[prevCarIndex + 1] = currentCar;
+  }
+
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
 }
